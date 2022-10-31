53d2d02ab0e92eb8e1411936c1f52ffed908098b
JabrilMahamud <Jabril.mahamud@bp.com>
2022-10-31 11:32:03 +0000

    tried

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,14 +6,14 @@ from .s3creator import *
 def index(response):
     return HttpResponse("First View")
 
-# class S3metadata(TemplateView):
-#     template_name = "/mysite/templates/s3metadata.html"
+# class S3metadata():
+#     template_name = "/mysite/templates/active.html"
 
 #     def get_context_data(self,**kwargs):
 #          context = super().get_context_data(**kwargs)
-#          context["dictionary"] = tableDict
+#          context["dictionary"] = s3creator("Active")
 #          return context
 
 
 def active(response):
-    return render(response,"/templates/active.html",s3creator("Active"))
+    return render(response,"mysite/templates/active.html")
