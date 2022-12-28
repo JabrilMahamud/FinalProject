9b8f3bf6d1570f364d3f1fc8d0e763016a82d8f9
JabrilMahamud <Jabril.mahamud@bp.com>
2022-12-28 13:27:19 +0000

    fixed active

--- mysite/main/classes/active.py
+++ mysite/main/classes/active.py
@@ -1,7 +1,7 @@
 import boto3
 from boto3.dynamodb.conditions import Attr
 
-def S3activeResponder(status):
+def S3activeResponder():
     dynamodb = boto3.resource("dynamodb", region_name='eu-west-2')
     table = dynamodb.Table('MetadataJson')
 
@@ -22,7 +22,6 @@ def S3activeResponder(status):
 
     for i in range(len(activeResponse)):
         activeAccounts.append([activeResponse[i].get('account-name'),activeResponse[i].get('account'),activeResponse[i].get('status')])
+    
 
     return activeAccounts
-
-
